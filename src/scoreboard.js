af8661195cb2dea4c73d3a400e3732e467d347d6
Kevin Zhou <kevinzhou22@outlook.com>
2021-01-24 22:38:17 +1100

    Make scoreboard module emit events upon changes to score or highScore

--- src/scoreboard.js
+++ src/scoreboard.js
@@ -1,16 +1,21 @@
+import events from './events';
+
 let score = 0;
 let highScore = 0;
 
 const incrementScore = function incrementScore() {
   score += 1;
+  events.emit('newScore', score);
 
   if (score > highScore) {
     highScore = score;
+    events.emit('newHighScore', highScore);
   }
 };
 
 const resetScore = function resetScore() {
   score = 0;
+  events.emit('newScore', score);
 };
 
 const getScore = function getScore() {
