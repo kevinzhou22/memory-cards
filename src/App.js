9068eb981eb84d3db67db83e131b2c5dfb044efd
Kevin Zhou <kevinzhou22@outlook.com>
2021-01-24 20:43:33 +1100

    Add style.css and very basic styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import './style.css';
 
 function App() {
   return (
+    <div className="app">
+      <h1>Memory Cards</h1>
+      { /* placeholder for scoreboard */}
+      <div />
+      { /* placeholder for cardsBoard */}
+      <div />
+    </div>
   );
 }
 
