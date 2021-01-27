ac5c59fd6cb3234ee96c7a2b7d26bc905a63461e
Kevin Zhou <kevinzhou22@outlook.com>
2021-01-27 12:08:39 +1100

    Insert CardBoard into App

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './reset.css';
 import './style.css';
 import ScoreboardDisplay from './components/ScoreboardDisplay';
+import CardBoard from './components/CardBoard';
 
 function App() {
   return (
     <div className="app">
       <h1>Memory Cards</h1>
       <ScoreboardDisplay />
-      { /* placeholder for cardsBoard */}
+      <CardBoard />
       <div />
     </div>
   );
